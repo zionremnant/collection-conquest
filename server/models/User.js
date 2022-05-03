4fe304a4f797505cbbb20552cd6299639f441ba4
Zion Yang <zionremnant@gmail.com>
2022-05-03 11:30:19 -0700

    added method validate pw

--- server/models/User.js
+++ server/models/User.js
@@ -4,37 +4,32 @@ const bcrypt = require("bcrypt");
 // import schema from Item.js
 const itemSchema = require("./Item");
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must match an email address!"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    // set item to be an array of data that adheres to the itemSchema
+    item: [itemSchema],
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, "Must match an email address!"],
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 5,
-  },
-  // set item to be an array of data that adheres to the itemSchema
-  item: [itemSchema],
-});
+  // set to use virtual below
+  { toJSON: { virtuals: true } }
+);
 
-userSchema.pre("save", async function (next) {
-  if (this.isNew || this.isModified("password")) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
-  }
-
-  next();
-});
-
+// custom method to compare and validate password for logging in
 userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
