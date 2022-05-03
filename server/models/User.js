82476c085e3cba741660ba3bba6565f6f44d8b84
Zion Yang <zionremnant@gmail.com>
2022-05-03 10:43:41 -0700

    updated user

--- server/models/User.js
+++ server/models/User.js
@@ -19,10 +19,10 @@ const userSchema = new Schema({
     required: true,
     minlength: 5,
   },
-  savedItem: [
+  item: [
     {
       type: Schema.Types.ObjectId,
-      ref: "savedItem",
+      ref: "Item",
     },
   ],
 });
