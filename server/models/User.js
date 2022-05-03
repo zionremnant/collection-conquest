0254aa0c45dbce0d514ac8211325b38b93a78d62
Zion Yang <zionremnant@gmail.com>
2022-05-03 12:12:59 -0700

    minor fixes

--- server/models/User.js
+++ server/models/User.js
@@ -28,15 +28,15 @@ const userSchema = new Schema(
   // set to use virtual below
   { toJSON: { virtuals: true } }
 );
-// // hash user pw
-// userSchema.pre('save', async function (next) {
-//   if (this.isNew || this.isModified('password')) {
-//     const saltRounds = 10;
-//     this.password = await bcrypt.hash(this.password, saltRounds);
-//   }
 
-//   next();
-// });
+// hash user pw
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
 
 // custom method to compare and validate password for logging in
 userSchema.methods.isCorrectPassword = async function (password) {
