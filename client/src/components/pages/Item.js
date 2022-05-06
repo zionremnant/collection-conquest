a4abd101eb8757e53eb8162a476f42d743c231b2
LinnetteCapul <linnette.capul@gmail.com>
2022-05-06 13:35:07 -0700

    styling for single item page

--- client/src/components/pages/Item.js
+++ client/src/components/pages/Item.js
@@ -1,6 +1,6 @@
 import { useQuery } from "@apollo/client";
 import { QUERY_ITEM } from "../../utils/queries";
-import { HStack } from "@chakra-ui/react";
+import { HStack, LinkBox, LinkOverlay, SimpleGrid, Text } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
 import { Box } from "@chakra-ui/react";
 import { Heading } from "@chakra-ui/react";
@@ -16,41 +16,55 @@ const Item = () => {
 
   return (
     <div>
-      <Heading>Welcome to Collection Conquest!!</Heading>
-      <div>
-        <h2>Here is the Collectible!</h2>
-        <HStack>
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <div>
-              <Box
-                bg="teal"
-                w="30rem"
-                borderWidth="1rem"
-                borderRadius="md"
-                borderColor="teal"
-                color="white"
-              >
-                <h2>{data.item.name}</h2>
-                <div>
-                  <Image
-                    boxSize="xl"
-                    src={data.item.imageURL}
-                    alt="Image Of Figure"
-                  />
-                </div>
-                <p>{data.item.description}</p>
-                <p>{data.item.dateOfPurchase}</p>
-                <p>{data.item.obtained}</p>
-                <p>{data.item.reminder}</p>
-                {/* <p>{data.item.user}</p> */}
-              </Box>
-            </div>
-          )}
-        </HStack>
-      </div>
-    </div>
+      <Box
+        w='100%'
+        h='100%'
+        bgGradient='linear( blue.200, green.300, cyan.600)'>
+        <Heading
+          bgGradient='linear(to-l, #004c4c, cyan.600)'
+          bgClip='text'
+          fontSize='6xl'
+          fontWeight='extrabold'
+
+
+
+        >Welcome to Collection Conquest!!</Heading>
+        <div>
+          <Text fontSize='2xl' > Here is the Collectible!!</Text>
+          <HStack>
+            {loading ? (
+              <div>Loading...</div>
+            ) : (
+              <div>
+                <Box
+                  bg="teal"
+                  w="30rem"
+                  borderWidth="1rem"
+                  borderRadius="md"
+                  borderColor="teal"
+                  color="white"
+                >
+                  <Text fontSize='4xl' as='u'> {data.item.name}</Text>
+
+                  <div>
+                    <Image
+                      boxSize="xl"
+                      src={data.item.imageURL}
+                      alt="Image Of Figure"
+                    />
+                  </div>
+                  <Text fontSize='2xl' > {data.item.description}</Text>
+                  <Text fontSize='1xl' > {data.item.dateOfPurchase}</Text>
+                  <Text fontSize='1xl' > {data.item.obtained}</Text>
+                  <Text fontSize='1xl' > {data.item.reminder}</Text>
+                  <Text fontSize='1xl' > {data.item.user}</Text>
+                </Box>
+              </div>
+            )}
+          </HStack>
+        </div>
+      </Box >
+    </div >
   );
 };
 
