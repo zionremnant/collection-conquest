6459f368f055c16737ef4e62132a1c7c53c18838
LinnetteCapul <linnette.capul@gmail.com>
2022-05-06 11:32:47 -0700

    css save point to pull from develop

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,11 @@
 import { useQuery } from "@apollo/client";
 import { QUERY_ITEMS } from "../../utils/queries";
-import { HStack, LinkBox, LinkOverlay } from "@chakra-ui/react";
+import { HStack, LinkBox, LinkOverlay, Grid, GridItem, SimpleGrid } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
 import { Box } from "@chakra-ui/react";
 import { Heading } from "@chakra-ui/react";
+
+
 
 const Home = () => {
     const { loading, data } = useQuery(QUERY_ITEMS, {
@@ -14,64 +16,75 @@ const Home = () => {
 
     return (
         <div>
-            <Heading
-                // w='100%'
-                h='50px'
-                bgGradient='linear(to-r, teal.500, green.300, cyan.600)'
+
+            <Box
+                w='100%'
+                h='100%'
+                bgGradient='linear( blue.100, green.300, cyan.600)'>
+                <Heading
+                    // w='100%'
+                    h='50px'
+                    bgGradient='linear(to-r, teal.500, green.300, cyan.600)'
 
 
-            // bgGradient='linear(to-l, cyan.600, green.300)'
-            // bgClip='text'
-            // fontSize='6xl'
-            // fontWeight='extrabold'
+                // bgGradient='linear(to-l, cyan.600, green.300)'
+                // bgClip='text'
+                // fontSize='6xl'
+                // fontWeight='extrabold'
 
 
+                >Welcome to Collection Conquest!!</Heading>
+                <div>
 
+                    <h2>Here is a list of the Collection so far!</h2>
+                    <HStack>
 
-            >Welcome to Collection Conquest!!</Heading>
-            <div>
-                <h2>Here is a list of the Collection so far!</h2>
-                <HStack>
-                    {loading ? (
-                        <div>Loading...</div>
-                    ) : (
-                        <div>
-                            <HStack>
-                                {itemList.map((item) => {
-                                    return (
-                                        <LinkBox>
-                                            <Box
-                                                bg="teal"
-                                                w="30rem"
-                                                borderWidth="1rem"
-                                                borderRadius="md"
-                                                borderColor="teal"
-                                                color="white"
+                        {loading ? (
+                            <div>Loading...</div>
+                        ) : (
+                            <div>
+                                <HStack>
+                                    <SimpleGrid columns={3} spacing={10}>
+                                        {itemList.map((item) => {
+                                            return (
+                                                <LinkBox>
+                                                    <Box
+                                                        bg="#086F83"
+                                                        w="30rem"
+                                                        borderWidth="1rem"
+                                                        borderRadius="md"
+                                                        borderColor="#086F83"
+                                                        color="white"
 
-                                            >
-                                                <h2>{item.name}</h2>
-                                                <div>
-                                                    <LinkOverlay href={`/Item/${item.name}`}>
-                                                        <Image
-                                                            boxSize="xl"
-                                                            src={item.imageURL}
-                                                            alt="Image Of Figure"
-                                                        />
-                                                    </LinkOverlay>
-                                                </div>
-                                                <p>{item.description}</p>
-                                                <p>{item.type}</p>
-                                                <p>{item.dateOfPurchase}</p>
-                                            </Box>
-                                        </LinkBox>
-                                    );
-                                })}
-                            </HStack>
-                        </div>
-                    )}
-                </HStack>
-            </div>
-        </div>
+                                                    >
+                                                        <h2>{item.name}</h2>
+                                                        <div>
+                                                            <LinkOverlay href={`/Item/${item.name}`}>
+                                                                <Image
+                                                                    boxSize="xl"
+                                                                    src={item.imageURL}
+                                                                    alt="Image Of Figure"
+                                                                />
+                                                            </LinkOverlay>
+                                                        </div>
+                                                        <p>{item.description}</p>
+                                                        <p>{item.type}</p>
+                                                        <p>{item.dateOfPurchase}</p>
+                                                    </Box>
+                                                </LinkBox>
+                                            );
+                                        })}
+                                    </SimpleGrid>
+                                </HStack>
+                            </div>
+                        )}
+
+                    </HStack>
+
+                </div>
+            </Box >
+
+        </div >
     );
 };
 
